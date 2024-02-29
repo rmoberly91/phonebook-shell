05393fb52b1fb69f1cab10b55f9b6c9486aa7789
rmoberly91 <84174538+rmoberly91@users.noreply.github.com>
2024-02-28 22:45:35 -0800

    Update phonebook.sh
    Added functions

--- phonebook.sh
+++ phonebook.sh
@@ -3,34 +3,50 @@
 file=phonebook.txt
 touch $file
 
-if [[ -z $1 ]]; then
-	echo "Invalid input"
-	exit 1
-fi
-
-if [[ $1 == *"new"* ]]; then
-	echo $2 $3 >> phonebook.txt
+new_user(){
+	
+	echo $2 $3  >> phonebook.txt
 	echo 'Contact added.'
+}
 
-elif [[ $1 == *"list"* ]]; then
+list_all(){
 	if [[ -s $file ]]; then
 		cat -n $file
 	else
 		echo "File is empty."
 	fi
-	
-elif [[ $1 == *"remove"* ]]; then
-	sed -i '/^'$2'/d' phonebook.txt
+}
+delete_entry(){
+	sed -i '/^'$1'/d' phonebook.txt
+}
 
-elif [[ $1 == *"clear"* ]]; then
-	rm -f $file 
+clear_file() {
+	rm -f $file
 	touch $file
-
-elif [[ (-z $2 && $1 != *"list"*) ]]; then
+}
+search_for(){
 	find=$(grep $1 $file)
 	if [[ (-z $find) ]]; then
 		echo 'No entry found.'
 	else 
 		echo $find 
 	fi
+}
+
+if [[ -z $1 ]]; then
+	echo "Invalid input"
+	exit 1
+fi
+
+if [[ $1 == *"new"* ]]; then
+	new_user $1 $2 $3
+elif [[ $1 == *"list"* ]]; then
+
+	list_all
+elif [[ $1 == *"remove"* ]]; then
+	delete_entry $2
+elif [[ $1 == *"clear"* ]]; then
+	clear_file
+elif [[ (-z $2 && $1 != *"list"*) ]]; then
+	search_for $1
 fi
